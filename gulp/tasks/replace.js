06695efdfa369846f29f67e0675a0d41acd5a5d4
yoichi kobayashi <info@tplh.net>
2017-08-24 00:24:46 +0900

    [modify] gulp replace task.

--- gulp/tasks/replace.js
+++ gulp/tasks/replace.js
@@ -5,7 +5,7 @@ const conf = require('../conf').replace;
 
 gulp.task('replaceHtml', () => {
   return gulp.src(conf.html.src)
-    .pipe($.replace(/(src="\/js\/)([a-z0-9_\.\-]*)(\.js")/g, '$1$2.min$3'))
-    .pipe($.replace(/(href="\/css\/)([a-z0-9_\.\-]*)(\.css")/g, '$1$2.min$3'))
+    .pipe($.replace(/(src="\/pjax\/js\/)([a-z0-9_\.\-]*)(\.js")/g, '$1$2.min$3'))
+    .pipe($.replace(/(href="\/pjax\/css\/)([a-z0-9_\.\-]*)(\.css")/g, '$1$2.min$3'))
     .pipe(gulp.dest(conf.html.dest));
 });
