d654fc306e617541053520c866c6333dfa2b03ac
yoichi kobayashi <info@tplh.net>
2018-02-15 01:29:04 +0900

    loadImgs function was became to return a load image array in a callback function.

--- src/js/modules/common/loadImgs.js
+++ src/js/modules/common/loadImgs.js
@@ -1,18 +1,21 @@
-export default function(imgArray, callback) {
-  const length = imgArray.length;
+export default function(imgArr, callback) {
+  const length = imgArr.length;
+  const loadedImgArr = [];
   let count = 0;
 
   for (var i = 0; i < length; i++) {
+    const index = i;
     const img = new Image();
     img.onload = () => {
+      loadedImgArr[index] = img;
       count++;
-      if (count >= length) callback();
+      if (count >= length) callback(loadedImgArr);
     };
     img.onerror = () => {
       console.error(`Failed to load image in loadImgs function.`)
       count++;
-      if (count >= length) callback();
+      if (count >= length) callback(loadedImgArr);
     };
-    img.src = imgArray[i];
+    img.src = imgArr[index];
   }
 }
