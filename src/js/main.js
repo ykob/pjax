29e5695654bc04af707a484046932a2eb6123f6c
yoichi kobayashi <info@tplh.net>
2018-04-02 00:32:32 +0900

    added a npm module viewport-units-buggyfill.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
 const ScrollManager = require('./modules/smooth_scroll_manager/SmoothScrollManager').default;
 const Pjax = require('./modules/pjax/Pjax').default;
 
@@ -5,6 +6,9 @@ const scrollManager = new ScrollManager();
 const pjax = new Pjax({
   scrollManager: scrollManager
 });
+
+// Apply the polyfill fixes the bug that Mobile Safari return a wrong vh value.
+viewportUnitsBuggyfill.init();
 
 const init = () => {
   pjax.onLoad();
