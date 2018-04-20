70b7f82deabb48cf76001967736ef9407b5b5e3c
yoichi kobayashi <info@tplh.net>
2018-04-21 03:31:56 +0900

    load a stylesheet ansynchronously in Pjax with preload.

--- src/js/main_with_preload.js
+++ src/js/main_with_preload.js
@@ -1,3 +1,4 @@
+const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
 const Pjax = require('./modules/pjax_with_preload/PjaxWithPreload').default;
 const ScrollManager = require('./modules/smooth_scroll_manager/SmoothScrollManager').default;
 
@@ -5,12 +6,20 @@ const coreModules = {
   pjax: new Pjax(),
   scrollManager: new ScrollManager(),
 };
+const ua = window.navigator.userAgent;
+const link = document.querySelector('link[as=style]');
 
 // connect core modules each other.
 coreModules.pjax.modules = coreModules;
 coreModules.scrollManager.modules = coreModules;
 
-coreModules.pjax.elm.progress.classList.add('is-shown');
+// preload stylesheet other than Google Chrome browser.
+if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
+
 setTimeout(() => {
-  coreModules.pjax.onLoad();
+  // start to run Pjax.
+  coreModules.pjax.elm.progress.classList.add('is-shown');
+  setTimeout(() => {
+    coreModules.pjax.onLoad();
+  }, 500);
 }, 100);
