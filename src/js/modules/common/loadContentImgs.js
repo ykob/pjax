0434f914c6f60aad87eede70fef7de4dd8e157e3
yoichi kobayashi <info@tplh.net>
2018-04-29 17:02:38 +0900

    added the third argument that sets additional images to loadContentImgs function module.

--- src/js/modules/common/loadContentImgs.js
+++ src/js/modules/common/loadContentImgs.js
@@ -1,12 +1,15 @@
 const loadImgs = require('./loadImgs').default;
 
-export default function(contents, callback) {
+export default function(contents, callback, addImgs = []) {
   const imgs = contents.querySelectorAll('img');
 
   if (imgs.length > 0) {
     const imgArray = [];
     for (var i = 0; i < imgs.length; i++) {
       imgArray[i] = imgs[i].src;
+    }
+    for (var j = 0; j < addImgs.length; j++) {
+      imgArray.push(addImgs[j]);
     }
     loadImgs(imgArray, callback);
   } else {
