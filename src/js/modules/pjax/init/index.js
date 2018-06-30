d31438db74152b16cbc762ab6a9faba3fb9c3067
yoichi kobayashi <info@tplh.net>
2018-06-30 23:45:41 +0900

    replaced loadImgs function to Promise.

--- src/js/modules/pjax/init/index.js
+++ src/js/modules/pjax/init/index.js
@@ -1,10 +1,16 @@
 require("babel-polyfill");
 
-const loadContentImgs = require('../../common/loadContentImgs').default;
+const loadImgs = require('../../common/loadImgs').default;
 
 module.exports = {
   // initBeforeTransit method: before scrollManager.resize run.
   initBeforeTransit: async function(contents, modules) {
+    await loadImgs([
+      '/pjax/img/index/bg.jpg',
+      '/pjax/img/page01/bg.jpg',
+      '/pjax/img/page02/bg.jpg',
+      '/pjax/img/page03/bg.jpg',
+    ]);
   },
   // initAfterTransit method: after scrollManager.resize run.
   initAfterTransit: function(contents, modules) {
