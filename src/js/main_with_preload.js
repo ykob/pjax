623e5b8b47190949f601adc3055701a94528c049
yoichi kobayashi <info@tplh.net>
2018-08-08 14:34:47 +0900

    changed init function to async function.

--- src/js/main_with_preload.js
+++ src/js/main_with_preload.js
@@ -1,28 +1,40 @@
 require("babel-polyfill");
 
 const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
+const sleep = require('js-util/sleep');
 const Pjax = require('./modules/pjax_with_preload/PjaxWithPreload').default;
 const ScrollManager = require('./modules/smooth_scroll_manager/SmoothScrollManager').default;
 const Renderer = require('./modules/common/Renderer').default;
 
 const modules = {
+  renderer: new Renderer(),
   pjax: new Pjax(),
   scrollManager: new ScrollManager(),
 };
-const renderer = new Renderer(modules);
 const ua = window.navigator.userAgent;
 const link = document.querySelector('link[as=style]');
 
 // connect core modules each other.
+modules.renderer.modules = modules;
 modules.pjax.modules = modules;
 modules.scrollManager.modules = modules;
 
-// preload stylesheet other than Google Chrome browser.
-if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
+const init = async () => {
+  // preload stylesheet other than Google Chrome browser.
+  if (ua.indexOf('Edge') > -1 || ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
 
-setTimeout(() => {
+  await sleep(100);
+
+  // Making viewport units (vh|vw|vmin|vmax) work properly in Mobile Safari.
+  viewportUnitsBuggyfill.init();
+
+  // add events.
+  modules.renderer.render = () => {
+    modules.scrollManager.render();
+  }
+
   // start to run Pjax.
-  modules.pjax.onLoad().then(() => {
-    renderer.start();
-  });
-}, 100);
+  await modules.pjax.onLoad();
+  modules.renderer.start();
+}
+init();
